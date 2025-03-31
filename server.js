0b722af9754946fad46dd1ac09e6f80947ce62cd
dat2k42004 <nguyenddat2410@gmail.com>
2025-03-31 23:26:34 +0700

    31/3/2025

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 3000;
 const server = createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\nNguyen Duc Dat');
+    res.end('Hello World');
 });
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
